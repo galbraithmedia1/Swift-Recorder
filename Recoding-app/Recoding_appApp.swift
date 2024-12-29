aab002a81d365aab5c5047c4677281f68f1a261e
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2024-12-28 22:02:17 -0700

    Initial Commit

---
+++ Recoding-app/Recoding_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recoding_appApp.swift
+//  Recoding-app
+//
+//  Created by Taylor Galbraith on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Recoding_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recoding-appTests/Recoding_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recoding_appTests.swift
+//  Recoding-appTests
+//
+//  Created by Taylor Galbraith on 12/28/24.
+//
+
+import Testing
+@testable import Recoding_app
+
+struct Recoding_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
